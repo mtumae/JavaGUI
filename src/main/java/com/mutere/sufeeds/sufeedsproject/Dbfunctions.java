cc0f8d263787a8acc1a1c2cccfa5be47b8859d08
mtume <mtume2016@gmail.com>
2024-11-05 06:14:12 +0300

    Adding Post page with populate data function

--- src/main/java/com/mutere/sufeeds/sufeedsproject/Dbfunctions.java
+++ src/main/java/com/mutere/sufeeds/sufeedsproject/Dbfunctions.java
@@ -1,9 +1,16 @@
 package com.mutere.sufeeds.sufeedsproject;
 
 
-import java.sql.*;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 
-public class Dbfunctions {
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+import java.util.Objects;
+
+
+public class Dbfunctions extends Home{
     public Connection connect_to_db(String dbname, String user, String pass) {
         Connection conn = null;
         try {
@@ -49,17 +56,68 @@ public class Dbfunctions {
     }
 
     //QUERYING STUDENT TABLE FOR SIGN IN
-    public void check_Student_ad_no(Connection conn, String ad_no) {
+    public void check_Student_ad_no(Connection conn, String ad_no, String pass) {
         Statement statement;
         try {
             String query = "select * from Students where ad_no='" + ad_no + "' limit 1;";
             statement = conn.createStatement();
             ResultSet result = statement.executeQuery(query);
             boolean exists = result.next();
-            System.out.println(exists+" User exists");
+            if(exists){
+                if(Objects.equals(pass, result.getString("password"))){
+                    System.out.println(exists+" User exists and password is correct" );
+                    message_box("Signed in successfully","Ok");
+
+                }
+                else{
+                System.out.println(exists+" User exists but password is not correct");
+                System.out.println(pass+" should equal "+result.getString("password")+" is your password");
+                message_box("Password is incorrect","Try again");
+                }
+            }
+            else{
+                System.out.println(exists+" User does not exist");
+            }
         } catch (Exception e) {
             System.out.println(e);
-            System.out.println("USer must not exist!");
+        }
+    }
+
+    public void retrieveUser(Connection conn, ActionEvent event, String ad_no) {
+        Statement statement;
+        try {
+            String query = "select * from Students where ad_no='" + ad_no + "' limit 1;";
+            statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(query);
+            while(result.next()) {
+                System.out.println("User retrieved! " + result.getString("name"));
+                //setNamelbl(result.getString("name"));
+                //setAd_nolbl(result.getString("ad_no"));
+            };
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void returnPosts(Connection conn, ActionEvent event){
+        ArrayList<String> posts = new ArrayList<String>();
+        ArrayList<String> posts_metadata = new ArrayList<String>();
+        ArrayList<String> posts_topic = new ArrayList<String>();
+        Statement statement;
+        try {
+            String query = "select * from Posts inner join Students using (ad_no)";
+            statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while(result.next()){
+                posts.add(result.getString("comment"));
+                posts_metadata.add(result.getString("ad_no")+" "+result.getString("name")+" "+result.getString("date_added"));
+                posts_topic.add(result.getString("topic"));
+            }
+            populateData(posts, posts_metadata, posts_topic, event);
+
+        }catch (Exception e){
+            System.out.println(e);
         }
     }
 }
--- src/main/java/com/mutere/sufeeds/sufeedsproject/Home.java
+++ src/main/java/com/mutere/sufeeds/sufeedsproject/Home.java
@@ -5,16 +5,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 
 public class Home extends Application {
@@ -22,7 +34,87 @@ public class Home extends Application {
     private Scene scene;
     private Parent root;
 
-    @FXML
+
+
+    public void message_box(String error_type, String button_text){
+        Dialog<String> dialog = new Dialog<String>();
+        dialog.setTitle("Error");
+        dialog.setContentText(error_type);
+        ButtonType type = new ButtonType(button_text, ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().add(type);
+        dialog.show();
+    }
+
+
+    public void populateData(ArrayList<String> posts, ArrayList<String> posts_metadata, ArrayList<String> posts_topics, ActionEvent event) throws IOException {
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        VBox layout = new VBox(20);
+        HBox filters = new HBox(10);
+        Label label = new Label("Blog");
+        Line line = new Line(10, 0, 600, 0);
+        layout.setAlignment(Pos.CENTER_LEFT);
+        layout.setPadding(new Insets(10));
+
+        //Nav bar
+        Button button1 = new Button("Home");
+        Button button2 = new Button("Post");
+        Button button3 = new Button("Blog");
+
+
+
+
+        // Create a navbar and add the buttons
+        HBox hbox = new HBox(10); // 10 is the spacing between buttons
+        hbox.getChildren().addAll(button1, button2, button3);
+        hbox.setAlignment(Pos.CENTER);
+
+
+        label.setStyle("-fx-font-weight: bold; -fx-font-size: 25;");
+        //layout.setStyle("-fx-background-color: #e0e0e0;" +
+                //"-fx-border-color: black; " +
+                //"-fx-border-width: 2px; " +
+                //"-fx-border-style: solid; " +
+                //
+        // "-fx-text-fill: white;");
+
+        layout.getChildren().add(hbox);
+        layout.getChildren().add(label);
+        layout.getChildren().add(filters);
+        layout.getChildren().add(line);
+
+        for (int i=0; i<posts.toArray().length; i++){
+            System.out.println(posts_metadata.get(i));
+            System.out.println(posts.get(i));
+            System.out.println(posts_topics.get(i));
+            VBox childVBox = new VBox(10);
+            Text text = new Text(posts.get(i));
+            Label label1 = new Label(posts_topics.get(i));
+            Label label2 = new Label(posts_metadata.get(i));
+            text.setStyle("-fx-font-size: 18; -fx-text-fill: black;");
+            label1.setStyle("-fx-font-weight: bold; -fx-font-size: 15; -fx-text-fill: black;");
+            label2.setStyle("-fx-font-size: 15; -fx-text-fill: gray;");
+            line.setStrokeWidth(2);
+
+
+
+            childVBox.getChildren().add(label1);
+            childVBox.getChildren().add(text);
+            childVBox.getChildren().add(label2);
+            layout.getChildren().add(childVBox);
+        }
+
+        ScrollPane scrollPane = new ScrollPane(layout);
+        scrollPane.setFitToHeight(true);
+        Scene scene = new Scene(scrollPane);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("Posts");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+
+
+
     public void switchtoLogin(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Signin.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -69,10 +161,17 @@ public class Home extends Application {
 
 
 
+
+
+
+
+
+
+
     @Override
     public void start(Stage stage) throws IOException {
         //change name to home
-        FXMLLoader fxmlLoader = new FXMLLoader(Home.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Home.class.getResource("Blog.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setTitle("SUFeeds");
@@ -85,3 +184,4 @@ public class Home extends Application {
         launch();
     }
 }
+
